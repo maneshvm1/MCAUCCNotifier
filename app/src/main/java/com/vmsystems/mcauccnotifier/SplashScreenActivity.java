6f07e38673b806cd0c41d2e7c4529e8ae53a8ce0
ManeshVM <maneshvmohanan@gmail.com>
2018-09-16 18:41:54 +0530

    logo ok splash screen partial

--- app/src/main/java/com/vmsystems/mcauccnotifier/SplashScreenActivity.java
+++ app/src/main/java/com/vmsystems/mcauccnotifier/SplashScreenActivity.java
@@ -1,13 +1,27 @@
 package com.vmsystems.mcauccnotifier;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),WelcomeActivity.class));
+                finish();
+            }
+        },1500);
     }
 }
